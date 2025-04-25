913f3b64fd5ef978bd05c8202cae88e97bcc68f7
LemonjamesD <lemon@lemonjamesd.com>
2025-04-25 02:56:17 -0400

    [SCRIPT] Updated dot files! 🚀

--- modules/system/autoflip-screen.nix
+++ modules/system/autoflip-screen.nix
@@ -1,16 +1,18 @@
 # This is crazy ass stuff I'm about to do
 # On the fent type shit
-{ pkgs, ... }:
+{ user, pkgs, ... }:
 
 {
   systemd.services.orientation-watcher = {
     description = "Orientation Change Handler";
     after = [ "network.target" ];
     wantedBy = [ "multi-user.target" ];
+    script = ''
+      ${pkgs.bash}/bin/bash /etc/nixos/scripts/autorotatelistener.sh
+    '';
     serviceConfig = {
-      ExecStart = ''
-        ${pkgs.bash}/bin/bash /etc/nixos/scripts/autorotatelistener.sh
-      '';
+      Type = "oneshot";
+      User = "${user}";
       Restart = "always";
       RestartSec = 5;
     };
