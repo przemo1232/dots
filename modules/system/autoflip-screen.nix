9cbaf8f79e9286705399b3b233e22c0bdadb7877
LemonjamesD <lemon@lemonjamesd.com>
2025-04-25 02:34:58 -0400

    [SCRIPT] Updated dot files! 🚀

--- modules/system/autoflip-screen.nix
+++ modules/system/autoflip-screen.nix
@@ -8,7 +8,21 @@
     after = [ "network.target" ];
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
-      ExecStart = "/etc/nixos/scripts/autorotatelistener.sh";
+      ExecStart = ''
+        ${pkgs.bash}/bin/bash -c '
+          monitor-sensor | grep --line-buffered "Accelerometer orientation changed:" | while read -r line; do
+              if [[ "$line" == "Accelerometer orientation changed: bottom-up" ]]; then
+                  echo "Detected orientation: not normal"
+                  # Replace this with your desired command for 'normal'
+                  hyprctl keyword monitor eDP-1,1920x1080,auto,1,transform,2
+              else
+                  echo "Detected orientation: normal"
+                  # Replace this with your desired command for other orientations
+                  hyprctl keyword monitor eDP-1,1920x1080,auto,1,transform,0
+              fi
+          done
+        '
+      '';
       Restart = "always";
       RestartSec = 5;
     };
