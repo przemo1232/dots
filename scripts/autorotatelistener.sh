c04d8f4d90b9e3017b6d24e134ee80825813b3cc
LemonjamesD <lemon@lemonjamesd.com>
2025-04-25 13:32:28 -0400

    [SCRIPT] Updated dot files! 🚀

--- scripts/autorotatelistener.sh
+++ scripts/autorotatelistener.sh
@@ -3,9 +3,12 @@ monitor-sensor | grep --line-buffered "Accelerometer orientation changed:" | whi
         echo "Detected orientation: not normal"
         # Replace this with your desired command for 'normal'
         hyprctl keyword monitor eDP-1,1920x1080,auto,1,transform,2
+        hyprctl keyword device:wacom-pen-and-multitouch-sensor-finger:transform 2
     else
         echo "Detected orientation: normal"
         # Replace this with your desired command for other orientations
         hyprctl keyword monitor eDP-1,1920x1080,auto,1,transform,0
+        hyprctl keyword device:wacom-pen-and-multitouch-sensor-finger:transform 0
     fi
+    ~/.config/hypr/scripts/switchkeyboard.sh
 done
---
+++ scripts/switchkeyboard.sh
@@ -0,0 +1,11 @@
+hyprctl switchxkblayout logitech-gaming-keyboard-g213 1
+hyprctl switchxkblayout razer-razer-huntsman-mini 1
+hyprctl switchxkblayout razer-razer-huntsman-mini-1 1
+hyprctl switchxkblayout keychron-keychron-v3
+hyprctl switchxkblayout keychron-keychron-v3 1
+hyprctl switchxkblayout keychron-keychron-v3-keyboard
+hyprctl switchxkblayout keychron-keychron-v3-keyboard 1
+hyprctl switchxkblayout thinkpad-extra-buttons
+hyprctl switchxkblayout thinkpad-extra-buttons 1
+hyprctl switchxkblayout at-translated-set-2-keyboard
+hyprctl switchxkblayout at-translated-set-2-keyboard 1
