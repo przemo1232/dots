4c50ad926049f7031eb09ebe21b3b367928129b2
Lemon <55360995+lemonlambda@users.noreply.github.com>
2025-06-04 12:20:01 -0400

    Delete secrets.nix

--- secrets.nix
+++
@@ -1,6 +0,0 @@
-{
-  passwd = ''$y$j9T$jQwKmJwvpnpTMCMNglGnP0$EPQ4kMqN2ktOOhbKoQqhHOgTQMa8z9KKyOo5gVjBoq0'';
-  git-key = ''06408C83157BC3925756EBFA1428E0EE4572A07B'';
-  spotify-username = "lemonjameslem@protonmail.com";
-  spotify-password = "--H{!U+(<S9k<4X{=0Q+H}z|~j1~GXnxFrhCNySOgm!Yw#W>h*`S_0C]v;{o-iI|";
-}
