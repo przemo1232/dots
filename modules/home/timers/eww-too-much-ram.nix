ab2186ad73c6dd5ac5316eed78df5efa20202b13
lambdalemon <lemonjameslem@protonmail.com>
2025-06-03 23:46:34 -0400

    [SCRIPT] Updated dot files! 🚀

--- modules/home/timers/eww-too-much-ram.nix
+++ modules/home/timers/eww-too-much-ram.nix
@@ -1,32 +1,30 @@
-{ pkgs, machine-settings, ... }: let
-  name =  builtins.trace "eww dir: ${pkgs.eww}/bin" "eww-using-too-much-ram";
+{ pkgs, machine-settings, ... }:
+
+let
+  name = "eww-using-too-much-ram";
 in {
   systemd.user.timers.${name} = {
+    Unit.Description = "Run ${name} every 15 minutes";
     Timer = {
       OnCalendar = "*:0/15";
       Persistent = true;
     };
-
-    Install = {
-      WantedBy = [ "timers.target" ];
-    };
+    Install.WantedBy = [ "timers.target" ];
   };
 
-
   systemd.user.services.${name} = {
+    Unit.Description = "Kill eww and reopen bar";
+
     Service = {
       Type = "oneshot";
 
       StandardOutput = "journal";
       StandardError = "journal";
-      
-      ExecStart = "${pkgs.bash}/bin/bash -c ''
-        echo '[eww] killing old instance'
-        pgrep -f .eww-wrapped | xargs --no-run-if-empty kill || true
-        echo '[eww] dispatching open bar'
-        hyprctl dispatch exec \"eww open bar\"
-      ''";
 
+      ExecStartPre = [
+        "${pkgs.procps}/bin/pgrep -f .eww-wrapped"
+      ];
+      ExecStart = "${pkgs.hyprland}/bin/hyprctl dispatch exec '${pkgs.eww}/bin/eww open bar'";
     };
   };
 }
