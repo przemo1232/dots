d4199cd076af441261900a1b042df0be54175a13
lambdalemon <lemonjameslem@protonmail.com>
2025-06-03 20:29:07 -0400

    [SCRIPT] Updated dot files! 🚀

--- settings/functions/mk-home-module-path.nix
+++ settings/functions/mk-home-module-path.nix
@@ -1,17 +1,20 @@
 let
-  inherit (builtins) toString;
-  inherit (import <nixpkgs/lib>) removePrefix;
+  # inherit (builtins) toString;
+  # inherit (import <nixpkgs/lib>) removePrefix;
 
-  machine-settings = import ../machine-settings.nix;
-  userPath = ../../home + "/${machine-settings.user}";
-  userPathStr = toString userPath;
+  # machine-settings = import ../machine-settings.nix;
+  # userPath = ../../home + "/${machine-settings.user}";
+  # # userPath = "";
+  # userPathStr = toString userPath;
 in
 map (f:
   let
-    fStr = toString f;
-    relative = removePrefix userPathStr fStr;
+    # fStr = toString f;
+    # relative = removePrefix userPathStr fStr;
+    # # relative = fStr;
   in
-    builtins.trace "userPath: ${userPathStr}"
-    builtins.trace "f       : ${fStr}"
-    ../../modules/home + relative
+    # builtins.trace "userPath: ${userPathStr}"
+    # builtins.trace "f       : ${fStr}"
+    # ../../modules/home + relative
+    f
 )
