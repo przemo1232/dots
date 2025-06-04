19a236aba43746ea164b374cef87cee2877943cd
lambdalemon <lemonjameslem@protonmail.com>
2025-06-03 22:56:41 -0400

    [SCRIPT] Updated dot files! 🚀

--- modules/home/timers/eww-too-much-ram.nix
+++ modules/home/timers/eww-too-much-ram.nix
@@ -17,7 +17,7 @@ in {
     Service = {
       Type = "oneshot";
       ExecStart = ''
-        ${pkgs.bash}/bin/bash -c 'kill $(pidof ".eww-wrapped") || true'
+        ${pkgs.bash}/bin/bash -c 'pkill ".eww-wrapped" || true'
         ${pkgs.eww}/bin/eww open bar
       '';
     };
