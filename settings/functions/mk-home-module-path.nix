f4c0d9fa56ff145b7d639c194e816c352ea73989
lambdalemon <lemonjameslem@protonmail.com>
2025-06-03 20:30:05 -0400

    [SCRIPT] Updated dot files! 🚀

--- settings/functions/mk-home-module-path.nix
+++ settings/functions/mk-home-module-path.nix
@@ -1,20 +1,3 @@
-let
-  # inherit (builtins) toString;
-  # inherit (import <nixpkgs/lib>) removePrefix;
-
-  # machine-settings = import ../machine-settings.nix;
-  # userPath = ../../home + "/${machine-settings.user}";
-  # # userPath = "";
-  # userPathStr = toString userPath;
-in
 map (f:
-  let
-    # fStr = toString f;
-    # relative = removePrefix userPathStr fStr;
-    # # relative = fStr;
-  in
-    # builtins.trace "userPath: ${userPathStr}"
-    # builtins.trace "f       : ${fStr}"
-    # ../../modules/home + relative
-    f
+  "/etc/nixos/modules/home/${joined}"
 )
