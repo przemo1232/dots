5113591dea643e96fab841b3c74ca58594b0ad4b
lambdalemon <lemonjameslem@protonmail.com>
2025-06-06 22:22:42 -0400

    Nvidia fix

--- modules/system/nvidia.nix
+++ modules/system/nvidia.nix
@@ -8,7 +8,9 @@
   hardware.nvidia.modesetting.enable = true;
   hardware.opengl = { # this fixes the "glXChooseVisual failed" bug, context: https://github.com/NixOS/nixpkgs/issues/47932
     enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
+    # driSupport = true;
+    # driSupport32Bit = true;
   };
+
+  hardware.nvidia.open = false;
 }
